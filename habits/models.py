8e8b69ba824605b1da4b54922402565c6b3ab5eb
DuckRainbow <sillestyna@gmail.com>
2024-11-04 15:39:24 +0500

    models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Habit(models.Model):
+  user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+  place = models.CharField(
+        max_length=50,
+        verbose_name='Место',
+        blank=True,
+        null=True,
+    )
+
+  time_where = models.DateTimeField(
+        verbose_name="Время",
+        blank=True,
+        null=True
+    )
+
+  action = models.CharField(
+        max_length=50,
+        verbose_name='Действие',
+        blank=True,
+        null=True,
+    )
+
+  is_pleasant = models.BooleanField()
+
+  related_habit = models.ForeignKey(
+        User,
+        verbose_name='Связанная привычка',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+  regularity =
+
+  reward = models.CharField(
+        max_length=50,
+        verbose_name='Вознаграждение',
+        blank=True,
+        null=True,
+    )
+
+  time_to_complete = models.DateTimeField(
+        verbose_name="Время на выполнение",
+        blank=True,
+        null=True
+    )
+
+  is_public = models.BooleanField()
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email'
+    )
+    token = models.CharField(
+        max_length=100,
+        verbose_name='Token',
+        blank=True,
+        null=True,
+    )
+    phone_num = models.IntegerField(
+        verbose_name='Phone',
+        blank=True,
+        null=True,
+    )
+    avatar = models.ImageField(
+        verbose_name='Avatar',
+        blank=True,
+        null=True,
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        permissions = [
+            ('can_block_user', 'Can block user')
+        ]
+
+    def __str__(self):
+        return self.email
